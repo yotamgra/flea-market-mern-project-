da539f63b86b7bd5a5f407d449d589968c207fa7
Yotam Granada <yotam.granada@gmail.com>
2022-12-24 16:34:31 +0200

    add token to registerUser and loginUser

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -38,27 +38,48 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user.id)
     });
   } else {
     res.status(400);
     throw new Error("Invalid user data");
   }
-
-
 });
 
 //@desc    Authenticate a user
 //@route   POST /users/login
 //@access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  //Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id)
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 //@desc    Get user data
 //@route   GET /users/me
-//@access  Public
+//@access  Private
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User data display" });
 });
+
+//Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 export { registerUser, loginUser, getMe };
