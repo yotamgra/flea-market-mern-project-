3beb0943e31c69bcb41683fa4f43119d3b21349c
Yotam Granada <yotam.granada@gmail.com>
2022-12-24 14:25:19 +0200

    connect updateItem function to MongoDB

--- backend/controllers/itemsController.js
+++ backend/controllers/itemsController.js
@@ -1,24 +1,23 @@
 import asyncHandler from "express-async-handler";
 
-import  Item  from "../models/itemModel.js";
+import Item from "../models/itemModel.js";
 
 //@desc    Get items
 //@route   GET /api/items
 //@access  Private
 const getItems = asyncHandler(async (req, res) => {
-    const items = await Item.find();
+  const items = await Item.find();
   res.status(200).json(items);
 });
 
 //@desc    Set item
 //@route   POST /api/items
 //@access  Private
-const setItems = asyncHandler(async (req, res) => {
-  if (!req.body.header || !req.body.price ) {
+const setItem = asyncHandler(async (req, res) => {
+  if (!req.body.header || !req.body.price) {
     res.status(400);
     throw new Error("Item header and price are required");
   }
-  console.log(Item);
   const item = await Item.create({
     header: req.body.header,
     price: req.body.price,
@@ -32,7 +31,15 @@ const setItems = asyncHandler(async (req, res) => {
 //@route   PUT /api/item/:id
 //@access  Private
 const updateItem = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update item ${req.params.id}` });
+    const item = await Item.findById(req.params.id);
+    if(!item){
+        res.status(400);
+    throw new Error("Item not found");
+    }
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      res.status(200).json(updatedItem);
 });
 
 //@desc    Delete item
@@ -42,4 +49,4 @@ const deleteItem = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete item ${req.params.id}` });
 });
 
-export { getItems, setItems, updateItem, deleteItem };
+export { getItems, setItem, updateItem, deleteItem };
--- backend/routes/itemsRoute.js
+++ backend/routes/itemsRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getItems, setItems, updateItem, deleteItem } from "../controllers/itemsController.js";
+import { getItems, setItem, updateItem, deleteItem } from "../controllers/itemsController.js";
 
 const router = express.Router();
 
-router.route("/").get(getItems).post(setItems);
+router.route("/").get(getItems).post(setItem);
 
 router.route("/:id").put(updateItem).delete(deleteItem);
 
