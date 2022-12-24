1dbc87c97cb31840aff88dfe90b0f83456e202ce
Yotam Granada <yotam.granada@gmail.com>
2022-12-24 16:17:34 +0200

    registerUser function connected to MongoDb

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,10 +1,50 @@
 import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
 
 //@desc    Register new user
 //@route   POST /users
 //@access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //Check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+
 });
 
 //@desc    Authenticate a user
@@ -20,6 +60,5 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User data display" });
 });
-
 
 export { registerUser, loginUser, getMe };
