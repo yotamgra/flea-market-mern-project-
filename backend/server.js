7b1555581dc78bb068fefb5305e89209615cede0
Yotam Granada <yotam.granada@gmail.com>
2023-01-02 16:01:04 +0200

    register and logout compleated

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,25 @@
 import express from "express";
-import colors from 'colors'
+import colors from "colors";
 import * as dotenv from "dotenv";
 import { itemsRoutes } from "./routes/itemsRoute.js";
 import { usersRoutes } from "./routes/usersRoutes.js";
-import { errorHandler } from './middleweare/errorMiddleware.js'
-import {connectDB} from './config/db.js'
+import { errorHandler } from "./middleweare/errorMiddleware.js";
+import { connectDB } from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
 const app = express();
-
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/items", itemsRoutes);
 app.use("/users", usersRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/users";
+
+//Register user
+const register = async (userData) => {
+  console.log(userData);
+  const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+//Logout user   
+const logout = async () => {
+    localStorage.removeItem('user')
+
+};
+
+const authService = {
+  register,
+  logout,
+};
+
+export default authService;
